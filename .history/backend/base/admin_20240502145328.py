952edf9b7a19c2ce4a6bd814a58e42d6cd009514
Nowacki97PL <noszo997@gmail.com>
2024-05-02 16:02:19 +0200

    Create Admin Panel part.1 - Users List

---
+++ .history/backend/base/admin_20240502145328.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Order, OrderItem, Product, Review, ShippingAddress
+
+class OrderAdmin(ModelAdmin):
+    ordering = ["id"]
+    list_display = [
+        "id",
+        "user",
+        "created_at"
+    ]
+    
+class UserAdmin(ModelAdmin):
+    ordering = ["id"]
+    list_display = {}
+
+
+admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+
+
