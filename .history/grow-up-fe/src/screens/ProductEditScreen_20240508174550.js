6d4337521caf6976c9a6e1c5d72992bf09ff6eba
Nowacki97PL <noszo997@gmail.com>
2024-05-08 17:54:11 +0200

    Add create product function and update product

---
+++ .history/grow-up-fe/src/screens/ProductEditScreen_20240508174550.js
@@ -0,0 +1,167 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import FormContainer from "../components/FormContainer";
+import { listProductDetail, updateProduct } from "../actions/productActions";
+import { PRODUCT_UPDATE_RESET } from "../constants/productConstants";
+
+function ProductEditScreen({}) {
+	const { id } = useParams();
+	const navigate = useNavigate();
+
+	const [name, setName] = useState("");
+	const [price, setPrice] = useState(0);
+	const [image, setImage] = useState("");
+	const [brand, setBrand] = useState("");
+	const [category, setCategory] = useState("");
+	const [count_in_stock, setCountInStock] = useState(0);
+	const [description, setDescription] = useState("");
+	const [uploading, setUploading] = useState(false);
+
+	const dispatch = useDispatch();
+
+	const productDetails = useSelector((state) => state.productDetails);
+	const { error, loading, product } = productDetails;
+
+	const productUpdate = useSelector((state) => state.productUpdate);
+	const {
+		error: errorUpdate,
+		loading: loadingUpdate,
+		success: successUpdate,
+	} = productUpdate;
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		dispatch(
+			updateProduct({
+				id: id,
+				name,
+				price,
+				image,
+				brand,
+				category,
+				count_in_stock,
+				description,
+			})
+		);
+	};
+
+	useEffect(() => {
+		if (successUpdate) {
+			dispatch({ type: PRODUCT_UPDATE_RESET });
+			navigate("/admin/product-list");
+		} else {
+			if (!product.name || product.id !== Number(id)) {
+				dispatch(listProductDetail(id));
+			} else {
+				setName(product.name);
+				setPrice(product.price);
+				setImage(product.image);
+				setBrand(product.brand);
+				setCategory(product.category);
+				setCountInStock(product.count_in_stock);
+				setDescription(product.description);
+			}
+		}
+	}, [dispatch, product, id, navigate, successUpdate]);
+
+	return (
+		<div>
+			<Link to="/admin/product-list">Powrót</Link>
+
+			<FormContainer>
+				<h1>Edytuj produkt</h1>
+				{loadingUpdate && <Loader />}
+				{errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
+
+				{loading ? (
+					<Loader />
+				) : error ? (
+					<Message variant="danger">{error}</Message>
+				) : (
+					<Form onSubmit={submitHandler}>
+						<Form.Group controlId="name">
+							<Form.Label>Nazwa</Form.Label>
+							<Form.Control
+								type="name"
+								placeholder="Wprowadź nazwę"
+								value={name}
+								onChange={(e) => setName(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+
+						<Form.Group controlId="price">
+							<Form.Label>Cena</Form.Label>
+							<Form.Control
+								type="number"
+								placeholder="Wprowadź cenę"
+								value={price}
+								onChange={(e) => setPrice(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+
+						<Form.Group controlId="image">
+							<Form.Label>Miniaturka</Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Załaduj obraz"
+								value={image}
+								onChange={(e) => setImage(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+
+						<Form.Group controlId="brand">
+							<Form.Label>Marka</Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Wprowadź markę"
+								value={brand}
+								onChange={(e) => setBrand(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+
+						<Form.Group controlId="countinstock">
+							<Form.Label>Stan magazynowy</Form.Label>
+							<Form.Control
+								type="number"
+								placeholder="Wprowadź stan magazynowy"
+								value={count_in_stock}
+								onChange={(e) => setCountInStock(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+
+						<Form.Group controlId="category">
+							<Form.Label>Kategoria</Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Wprowadź kategorię"
+								value={category}
+								onChange={(e) => setCategory(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+
+						<Form.Group controlId="description">
+							<Form.Label>Opis</Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Wprowadź opis"
+								value={description}
+								onChange={(e) => setDescription(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+
+						<Button className="mt-2" type="submit" variant="primary">
+							Aktualizuj
+						</Button>
+					</Form>
+				)}
+			</FormContainer>
+		</div>
+	);
+}
+
+export default ProductEditScreen;
---
+++ .history/grow-up-fe/src/screens/ProductListScreen_20240508162213.js
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import {
+	listProducts,
+	deleteProduct,
+	createProduct,
+} from "../actions/productActions";
+import { PRODUCT_CREATE_RESET } from "../constants/productConstants";
+
+function ProductListScreen() {
+	const dispatch = useDispatch();
+
+	const productList = useSelector((state) => state.productList);
+	const { loading, error, products } = productList;
+
+	const productDelete = useSelector((state) => state.productDelete);
+	const {
+		loading: loadingDelete,
+		error: errorDelete,
+		success: successDelete,
+	} = productDelete;
+
+	const productCreate = useSelector((state) => state.productCreate);
+	const {
+		loading: loadingCreate,
+		error: errorCreate,
+		success: successCreate,
+		product: createdProduct,
+	} = productCreate;
+
+	const userLogin = useSelector((state) => state.userLogin);
+	const { userInfo } = userLogin;
+
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		dispatch({ type: PRODUCT_CREATE_RESET })
+		if (userInfo.isAdmin) {
+			navigate
+		} else {
+			navigate("/login");
+		}
+	}, [dispatch, userInfo, navigate, successDelete]);
+
+	const createProductHandler = (product) => {
+		//create product
+	};
+
+	const deleteHandler = (id) => {
+		if (window.confirm("Jesteś pewny, że chcesz usunąć ten produkt?"))
+			dispatch(deleteProduct(id));
+	};
+
+	return (
+		<div>
+			<Row className="align-items-center">
+				<Col>
+					<h1>Produkty</h1>
+				</Col>
+				<Col className="text-end">
+					<Button className="my-3" onClick={createProductHandler}>
+						<i className="fas fa-plus"></i> Dodaj produkt
+					</Button>
+				</Col>
+			</Row>
+
+			{loadingDelete && <Loader />}
+			{errorDelete && <Message variant="danger">{errorDelete}</Message>}
+
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant="danger">{error}</Message>
+			) : (
+				<Table striped bordered hover responsive className="table-sm">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>IMIĘ</th>
+							<th>CENA</th>
+							<th>KATEGORIA</th>
+							<th>MARKA</th>
+							<th></th>
+						</tr>
+					</thead>
+
+					<tbody>
+						{products.map((product) => (
+							<tr key={product.id}>
+								<td>{product.id}</td>
+								<td>{product.name}</td>
+								<td>{product.price}</td>
+								<td>{product.category}</td>
+								<td>{product.brand}</td>
+								<td>
+									<LinkContainer to={`/admin/product/${product.id}/edit`}>
+										<Button variant="light" className="btn-sm">
+											<i className="fas fa-edit"></i>
+										</Button>
+									</LinkContainer>
+
+									<Button
+										variant="danger"
+										className="btn-sm"
+										onClick={() => deleteHandler(product.id)}
+									>
+										<i className="fas fa-trash"></i>
+									</Button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</Table>
+			)}
+		</div>
+	);
+}
+
+export default ProductListScreen;
